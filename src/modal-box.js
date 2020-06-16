210331c00251603230dd2d805142e070bbf76380
luanhaipeng <luanhaipeng@staff.beechat.io>
2020-06-16 17:08:46 +0800

    animationClass

--- src/modal-box.js
+++ src/modal-box.js
@@ -1,6 +1,7 @@
 require('./modal-box.less');
 
 var ICON_ERROR = `<svg xmlns="http://www.w3.org/2000/svg"  viewBox="64 64 896 896" class="" data-icon="close-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false"><path d="M685.4 354.8c0-4.4-3.6-8-8-8l-66 .3L512 465.6l-99.3-118.4-66.1-.3c-4.4 0-8 3.5-8 8 0 1.9.7 3.7 1.9 5.2l130.1 155L340.5 670a8.32 8.32 0 0 0-1.9 5.2c0 4.4 3.6 8 8 8l66.1-.3L512 564.4l99.3 118.4 66 .3c4.4 0 8-3.5 8-8 0-1.9-.7-3.7-1.9-5.2L553.5 515l130.1-155c1.2-1.4 1.8-3.3 1.8-5.2z"></path><path d="M512 65C264.6 65 64 265.6 64 513s200.6 448 448 448 448-200.6 448-448S759.4 65 512 65zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path></svg>`;
+let ICON_INFO = `<svg viewBox="64 64 896 896" focusable="false" class="" data-icon="info-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm32 664c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V456c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272zm-32-344a48.01 48.01 0 0 1 0-96 48.01 48.01 0 0 1 0 96z"></path></svg>`;
 
 var MODAL_BOX_ID = "tiny-message-modal-box";
 
@@ -17,8 +18,8 @@ function getDOMElement() {
             '        <div class="tiny-message-modal-content"></div>' +
             '    </div>' +
             '    <div class="tiny-message-modal-buttons">' +
-            '        <button class="tiny-message-modal-btn-cancel"></button>' +
             '        <button class="tiny-message-modal-btn-ok"></button>' +
+            '        <button class="tiny-message-modal-btn-cancel"></button>' +
             '    </div>' +
             '</div>';
         document.body.appendChild(dom);
@@ -48,7 +49,9 @@ function showModalBox(config) {
     var dom = getDOMElement();
 
     var iconDom = dom.getElementsByClassName('tiny-message-modal-icon')[0];
-    if (icon === 'error') {
+    if (icon === 'info'){
+        iconDom.innerHTML = `<span style="color: #1890ff">${ICON_INFO}</span>`;
+    } else if (icon === 'error') {
         iconDom.innerHTML = ICON_ERROR;
     } else {
         iconDom.innerHTML = icon;
