05dfdd263e31d7265c2f9c59aed3cc31db9eec21
luanhaipeng <luanhaipeng@staff.beechat.io>
2020-11-20 20:34:15 +0800

    d.ts

--- tinymessage.d.ts
+++ tinymessage.d.ts
@@ -13,27 +13,27 @@ export interface ModalBoxConfig {
     cancelClick?: (e?: any) => void,
 }
 
-export declare function setUseFrameProxy(isSend: boolean, isListen: boolean): void;
+export declare function setUseFrameProxy(isSend?: boolean, isListen?: boolean): void;
 
-export declare function info(content: string, duration: number): MessageObject;
+export declare function info(content: string, duration?: number): MessageObject;
 
-export declare function success(content: string, duration: number): MessageObject;
+export declare function success(content: string, duration?: number): MessageObject;
 
-export declare function error(content: string, duration: number): MessageObject;
+export declare function error(content: string, duration?: number): MessageObject;
 
-export declare function warn(content: string, duration: number): MessageObject;
+export declare function warn(content: string, duration?: number): MessageObject;
 
-export declare function warning(content: string, duration: number): MessageObject;
+export declare function warning(content: string, duration?: number): MessageObject;
 
-export declare function loading(content: string, duration: number): MessageObject;
+export declare function loading(content: string, duration?: number): MessageObject;
 
 export declare function destroy(): void;
 
-export declare function toastSuccess(content: string, duration: number): void;
+export declare function toastSuccess(content: string, duration?: number): void;
 
-export declare function toastLoading(content: string, duration: number): void;
+export declare function toastLoading(content: string, duration?: number): void;
 
-export declare function toast(content: string, duration: number, animationClass: string): void;
+export declare function toast(content: string, duration?: number, animationClass?: string): void;
 
 export declare function showModalBox(config: ModalBoxConfig): void;
 
